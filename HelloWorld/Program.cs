e2f201719076b57a43fd4e4c98f7c4348b30a9ff
Suma Teki <sumateki5@gmail.com>
2025-02-11 12:25:42 +0530

    Updated the code.

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -8,13 +8,7 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("What is your name? ");
-            var name = Console.ReadLine();
-            var today = DateTime.Now;
-            Console.WriteLine($"{Environment.NewLine}Hello, {name}, on {DateTime.Now:d} at {DateTime.Now:t}!");
-            
-            Console.Write($"{ Environment.NewLine}Press any key to exit...");
-            Console.ReadKey(true);
+            Console.WriteLine("Hello World...!!!");
         }
     }
 }
